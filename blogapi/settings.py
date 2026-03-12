80999e9d19d4408c3582e7057840ac32417424e7
mirzaatif89 <mirzaatif7809@gmail.com>
2026-03-13 00:24:36 +0500

    cores added with *

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'blog',
     'ckeditor',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,6 +136,9 @@ CKEDITOR_CONFIGS = {
         'extraPlugins': ','.join(['uploadimage', 'image2']),
     },
 }
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
